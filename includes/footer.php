25dba31ea15cfb1f334222f7c4b6850b960658fc
RADHIKA JOSHI <65120628+RADHIKA-JOSHI123@users.noreply.github.com>
2021-07-01 15:15:46 +0530

    fyp

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<footer class="footer-area section-gap">
+        <div class="container">
+          
+            <div class="footer-bottom row align-items-center text-center text-lg-left">
+                <p class="footer-text m-0 col-lg-8 col-md-12"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+ <i class="ti-heart" aria-hidden="true"></i> by <a href="https://gppune.ac.in/userindex.php/" target="_blank">made by -Radhika & Anam</a>
+</p>
+            </div>
+        </div>
+    </footer>
